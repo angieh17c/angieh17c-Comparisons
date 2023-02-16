a9fa266fe37bb27b224a7825df395187364b71bd
angieh17c <angieh17c@gmail.com>
2023-02-16 05:12:22 +0000

    Completed Lab Comparisions

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -23,25 +23,41 @@ public class Lab {
      * @param b the second number to be compared.
      * @return true if a is equal to b, false otherwise.
      */
+    boolean check = true;
     public boolean isEqual(int a, int b){
-        return false;
+        if (a == b){
+            check = true;
+        } else{
+        check = false;
+        }
+        return check;
     }
-
     /**
      * @param a the first number to be compared.
      * @param b the second number to be compared.
      * @return true if a is greater than b, false otherwise.
      */
     public boolean greaterThan(int a, int b){
-        return false;
-    }
+        if (a > b){
+            check = true;
+        } else{
+        check = false;
+        }
+        return check;
+        }
+    
     /**
      * @param a the first number to be compared.
      * @param b the second number to be compared.
      * @return true if a is less than b, false otherwise.
      */
     public boolean lessThan(int a, int b){
-        return false;
+        if (a < b){
+            check = true;
+        } else{
+        check = false;
+        }
+        return check;
     }
     /**
      * @param a the first number to be compared.
@@ -49,7 +65,12 @@ public class Lab {
      * @return true if a is greater than or equal to b, false otherwise.
      */
     public boolean greaterThanOrEqualTo(int a, int b){
-        return false;
+        if (a >= b){
+            check = true;
+        } else{
+        check = false;
+        }
+        return check;
     }
     /**
      * @param a the first number to be compared.
@@ -57,8 +78,13 @@ public class Lab {
      * @return true if a is less than or equal to b, false otherwise.
      */
     public boolean lessThanOrEqualTo(int a, int b){
-        return false;
-    }
+        if (a <= b){
+           check = true;
+       } else{
+       check = false;
+       }
+       return check;
+   }
 
     /**
      * @param a the first number to be compared.
@@ -66,6 +92,11 @@ public class Lab {
      * @return true if a is not equal to b, false otherwise.
      */
     public boolean notEqualTo(int a, int b){
-        return false;
+        if (a != b){
+            check = true;
+        } else{
+        check = false;
+        }
+        return check;
     }
 }
